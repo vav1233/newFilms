f55ebd42f0c9be7f8aa13b4f47623962d65740db
vav1233 <taniagayzler@gmail.com>
2022-05-03 14:03:08 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const swiper = new Swiper('.swiper', {
+   // If we need pagination
+   pagination: {
+      el: '.swiper-pagination',
+   },
+
+   // Navigation arrows
+   navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+   },
+
+   // And if we need scrollbar
+   scrollbar: {
+      el: '.swiper-scrollbar',
+   },
+});
